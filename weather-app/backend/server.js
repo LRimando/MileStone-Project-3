629cf0076c83af5ff0f94362abd7c4c046a808e9
guillermo rodriguez <guillermordgz.96@gmail.com>
2023-06-23 20:37:35 -0700

    user

--- weather-app/backend/server.js
+++ weather-app/backend/server.js
@@ -3,8 +3,27 @@ const app = express();
 const port = 8000;
 const axios = require('axios');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 app.use(cors());
+
+
+const authenticateToken = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ error: 'Authentication required' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, 'your_secret_key');
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: 'Invalid token' });
+  }
+};
+
 
 app.get('/weather', (req, res) => {
   const { location } = req.query;
@@ -32,4 +51,41 @@ app.get('*', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+
+app.post('/register', async (req, res) => {
+  const { username, password, email } = req.body;
+
+  try {
+    const user = new User({ username, password, email });
+    await user.save();
+    res.status(200).json({ message: 'Registration successful' });
+  } catch (error) {
+    res.status(500).json({ error: 'Registration failed' });
+  }
+});
+
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user || password !== user.password) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    const token = jwt.sign({ userId: user._id }, '42069');
+    res.cookie('token', token, { httpOnly: true }); // Store the token in a secure cookie
+    res.status(200).json({ message: 'Login successful', token });
+  } catch (error) {
+    res.status(500).json({ error: 'Login failed' });
+  }
+});
+
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('token'); // Clear the token cookie
+  res.status(200).json({ message: 'Logout successful' });
 });
